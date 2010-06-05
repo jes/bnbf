20877d9cd05a3fcf0427ea8bd2fb3c9c1a1280fc
James Stanley <james@incoherency.co.uk>
2010-06-05 17:28:28 +0100

    Initial code for new internal bignum

--- PKGBUILD
+++ PKGBUILD
@@ -5,5 +5,3 @@ pkgver=0.1
 pkgrel=1
 pkgdesc="Bignum brainfuck interpreter with various runtime options"
 arch=('i686' 'x86_64')
-url="http://aw.eso.me.uk/bnbf/"
-
--- src/bnbf.h
+++ src/bnbf.h
@@ -13,7 +13,6 @@
 #include "config.h"
 
 /* options.c */
-extern struct argp argp;
 extern int benchmark;
 extern int chario;
 extern char *eof_value;
@@ -37,7 +36,6 @@ void run_program(const char *name);
 void free_program(Inst *prog);
 
 /* memory.c */
-#define POS 0
 typedef struct Memory {
   int mp;/* memory pointer */
   mpz_t *pos_mem;/* positive-address memory */
@@ -52,3 +50,13 @@ void input(Memory *mem);
 void output(Memory *mem);
 int is_zero(Memory *mem);
 void free_memory(Memory *mem);
+
+/* bignum.c */
+typedef struct Bignum {
+  char *byte;
+  int num_bytes;
+} Bignum;
+
+Bignum *new_bignum(void);
+void add_bignum(Bignum *b, long n);
+void print_bignum(Bignum *b, FILE *fp);
